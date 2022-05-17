ea29043851df15ea0af9852bb7888fca7ee3a674
Brian Ronan <brian@xetdata.com>
2022-05-17 13:28:06 -0700

    Renaming xetcmd->xet-tools.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew Tap for Xet-Tools
+
+## Installation Steps
+
+We deliver our binaries as a third-party tap through Homebrew. In order to install, please run the following commands:
+
+```
+> brew tap xetdata/xet-tools
+> brew install xet-tools
+```
---
+++ xet-tools.rb
@@ -0,0 +1,12 @@
+class Xetcmd < Formula
+  desc "XetHub CLI Tools Homebrew Tap."
+  homepage "https://github.com/xethub/xet-tools"
+  url "https://github.com/xetdata/xet-tools/releases/download/v0.1.0-test2/xet-v0.1.0-test2-mac-universal.tar.gz"
+  sha256 "efb5fec1286524af543fa0377ffffe7a8044e753b8b1921b69f142d75ec04f8e"
+  license :cannot_represent
+
+  def install
+    bin.install 'xet'
+  end
+
+end
