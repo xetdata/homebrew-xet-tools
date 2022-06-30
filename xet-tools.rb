04c0eafa93853641a2e377af234a3d5263e65180
XetData Automation <operations@xetdata.com>
2022-06-30 23:38:29 +0000

    v0.5.0

--- xet-tools.rb
+++ xet-tools.rb
@@ -1,12 +1,14 @@
 class XetTools < Formula
   desc "XetHub CLI Tools Homebrew Tap."
   homepage "https://github.com/xethub/xet-tools"
-  url "https://github.com/xetdata/xet-tools/releases/download/v0.2.1/xet-v0.2.1-mac-universal.tar.gz"
-  sha256 "4d37145c92f32015aed9705dc171faf4468deb114620b5f0de7411a943219f7c"
+  url "https://github.com/xetdata/xet-tools/releases/download/v0.5.0/xet-v0.5.0-mac-universal.tar.gz"
+  sha256 "01ac3b6bef614f13ad504c398d8acfb0561f7936cb23b66045bba5bba4149363"
   license :cannot_represent
 
   def install
     bin.install 'xet'
+    bin.install 'git-xet'
+    system 'git-xet', 'install'
   end
 
 end
