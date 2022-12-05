0989a9c89f098daf06bdc3a2d23f4adf50174be4
XetData Automation <operations@xetdata.com>
2022-12-05 23:14:18 +0000

    0.7.0

--- xet-tools.rb
+++ xet-tools.rb
@@ -6,13 +6,13 @@ class XetTools < Formula
   license :cannot_represent
 
   on_macos do
-    url "https://github.com/xetdata/xet-tools/releases/download/v0.6.3/xet-v0.6.3-mac-universal.tar.gz"
-    sha256 "810f1f0ad5f0c5d7a75ad1922d98737829c2629a248c0d3c072a6abaa7eb0989"
+    url "https://github.com/xetdata/xet-tools/releases/download/0.7.0/xet-0.7.0-mac-universal.tar.gz"
+    sha256 "7bc525d6b97982cf96f297f77eaa216a77791d7e2d96302cc9c32778d49c32d5"
   end
 
   on_linux do
-    url "https://github.com/xetdata/xet-tools/releases/download/v0.6.3/xet-v0.6.3-linux-x86_64.tar.gz"
-    sha256 "4ff670f66c9cae43867d0c6d520db449a4b9791d544d3bc505f79a0baa8a68cc"
+    url "https://github.com/xetdata/xet-tools/releases/download/0.7.0/xet-0.7.0-linux-x86_64.tar.gz"
+    sha256 "d731d8507bed4f26ea95ac14ab3d8f3eec247fed7738e36a77e993a7374d7d91"
   end
 
   def caveats
