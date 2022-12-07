dcc23bf2d3646522d8b19ace1f46b768150fb183
Joseph Godlewski <jgodlewski@xetdata.com>
2022-12-07 14:53:21 -0800

    0.7.0

--- xet-tools.rb
+++ xet-tools.rb
@@ -6,12 +6,12 @@ class XetTools < Formula
   license :cannot_represent
 
   on_macos do
-    url "https://github.com/xetdata/xet-tools/releases/download/0.7.0/xet-0.7.0-mac-universal.tar.gz"
+    url "https://github.com/xetdata/xet-tools/releases/download/v0.7.0/xet-v0.7.0-mac-universal.tar.gz"
     sha256 "7bc525d6b97982cf96f297f77eaa216a77791d7e2d96302cc9c32778d49c32d5"
   end
 
   on_linux do
-    url "https://github.com/xetdata/xet-tools/releases/download/0.7.0/xet-0.7.0-linux-x86_64.tar.gz"
+    url "https://github.com/xetdata/xet-tools/releases/download/v0.7.0/xet-v0.7.0-linux-x86_64.tar.gz"
     sha256 "d731d8507bed4f26ea95ac14ab3d8f3eec247fed7738e36a77e993a7374d7d91"
   end
 
