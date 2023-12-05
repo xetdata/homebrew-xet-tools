ca324233494ef6265125f260d453f14cf81ebe79
XetData Automation <operations@xetdata.com>
2023-12-05 23:05:47 +0000

    v0.12.6

--- xet-tools.rb
+++ xet-tools.rb
@@ -2,16 +2,16 @@ class XetTools < Formula
   desc "XetHub CLI Tools Homebrew Tap."
   homepage "https://github.com/xetdata/xet-tools"
   license :cannot_represent
-  version "0.12.5"
+  version "0.12.6"
 
   on_macos do
-    url "https://github.com/xetdata/xet-tools/releases/download/v0.12.5/xet-mac-universal.tar.gz"
-    sha256 "2e209ecfc45cd72155eb9854b7edbc789aa0417642bd57b99e88305a6000c06f"
+    url "https://github.com/xetdata/xet-tools/releases/download/v0.12.6/xet-mac-universal.tar.gz"
+    sha256 "896809d8a87e14f2ca0e248025d7b22aeab03634449387244f45f93b9bf75270"
   end
 
   on_linux do
-    url "https://github.com/xetdata/xet-tools/releases/download/v0.12.5/xet-linux-x86_64.tar.gz"
-    sha256 "d4dd2812a221934e2c88bd42d12e928254cbf53486a0634bb43894e27e2c1a54"
+    url "https://github.com/xetdata/xet-tools/releases/download/v0.12.6/xet-linux-x86_64.tar.gz"
+    sha256 "bb6b194fa2e707a1059ec32c8fef2307debc3e46b6c72f00e6e8cab7e8ba6ad6"
   end
 
   def caveats
